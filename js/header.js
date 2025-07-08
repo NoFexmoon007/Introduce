c10ca38a351321b058199d4e9babc85f9adb53f8
note <fexmoon007@gmail.com>
2025-07-09 04:35:01 +0700

    idk what i did but i think i fix theme toggle button

--- js/header.js
+++ js/header.js
@@ -1,15 +1,16 @@
 const toggleButton = document.getElementById('Theme-toggles');
 const body = document.body;
 const header = document.querySelector('header');
-const li = document.querySelectorAll('header nav ul li a');
+const white = document.querySelectorAll('header nav ul li a');
 
 function switchTheme() {
 
     toggleButton.classList.toggle('active');
     body.classList.toggle('dark-mode');
     header.classList.toggle('toggle');
-    li.classList.toggle('fontwhite');
-
-}
+    white.forEach(function (link) {
+        link.classList.toggle('fontwhite');
+    });
+};  
 
 toggleButton.addEventListener('click', switchTheme);
