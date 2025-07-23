2d6a80c49d6cfbe0b50433e35a051530a79fc0a3
Com <fexmoon007@gmail.com>
2025-07-24 01:27:34 +0700

    switch to laptop

--- js/header.js
+++ js/header.js
@@ -2,15 +2,13 @@ const toggleButton = document.getElementById('Theme-toggles');
 const body = document.body;
 const header = document.querySelector('header');
 const white = document.querySelectorAll('header nav ul li a');
-const searchBar = document.getElementById('search-bar');
-const searchBarDiv = document.getElementById('SearchBar');
 
-// const searchBar = document.getElementById('search-bar');
-// const searchInput = document.getElementById("search-input");
-// const searchButton = document.querySelector('.search-bar button');
+
+const searchBar = document.getElementById('search-bar-container');
+const searchInput = document.getElementById('search-input');
+const searchButton = document.getElementById('search-button');
 
 function switchTheme() {
-
     toggleButton.classList.toggle('active');
     body.classList.toggle('dark-mode');
     header.classList.toggle('dark-mode');
@@ -18,10 +16,11 @@ function switchTheme() {
         link.classList.toggle('fontwhite');
     });
 
-    
-    //searchBar.classList.toggle('dark-mode');
-    //searchInput.classList.toggle('dark-mode');
-    //searchButton.classList.toggle('dark-mode');
-};  
+
+    searchBar.classList.toggle('dark-mode');
+    searchInput.classList.toggle('dark-mode');
+    searchButton.classList.toggle('dark-mode');
+
+}
 
 toggleButton.addEventListener('click', switchTheme);
--- js/search.js
+++ js/search.js
@@ -1,12 +1,11 @@
-const searchBar = document.getElementById('search-bar');
+const searchForm = document.getElementById('search-bar-container');
 const searchInput = document.getElementById("search-input");
 
-searchBar.addEventListener("submit", function(event) {
-    event.preventDefault(); // Prevent the default form submission
+searchForm.addEventListener("submit", function(event) {
+    event.preventDefault();
     const searchTerm = searchInput.value.trim().toLowerCase();
 
     if (searchTerm) {
-      // แทนที่จะส่งไป Google เราจะแสดงเป็นกล่องข้อความแทน
       alert(`คุณกำลังค้นหาคำว่า: ${searchTerm}`);
     }
 });
