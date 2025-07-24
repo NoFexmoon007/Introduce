93fc6e41d8f10316e625115962de2cd3b924a8d6
note <fexmoon007@gmail.com>
2025-07-24 17:37:00 +0700

    fix bug js and css

--- js/header.js
+++ js/header.js
@@ -1,26 +1,25 @@
-const toggleButton = document.getElementById('Theme-toggles');
-const body = document.body;
-const header = document.querySelector('header');
-const white = document.querySelectorAll('header nav ul li a');
+(() => {
+    const toggleButton = document.getElementById('Theme-toggles');
+    const body = document.body;
+    const header = document.querySelector('header');
+    const white = document.querySelectorAll('header nav ul li a');
 
+    const searchBar = document.getElementById('search-bar-container');
+    const searchInput = document.getElementById('search-input');
+    const searchButton = document.getElementById('search-button');
 
-const searchBar = document.getElementById('search-bar-container');
-const searchInput = document.getElementById('search-input');
-const searchButton = document.getElementById('search-button');
+    function switchTheme() {
+        toggleButton.classList.toggle('active');
+        body.classList.toggle('dark-mode');
+        header.classList.toggle('dark-mode');
+        white.forEach(function (link) {
+            link.classList.toggle('fontwhite');
+        });
 
-function switchTheme() {
-    toggleButton.classList.toggle('active');
-    body.classList.toggle('dark-mode');
-    header.classList.toggle('dark-mode');
-    white.forEach(function (link) {
-        link.classList.toggle('fontwhite');
-    });
+        searchBar.classList.toggle('dark-mode');
+        searchInput.classList.toggle('dark-mode');
+        searchButton.classList.toggle('dark-mode');
+    }
 
-
-    searchBar.classList.toggle('dark-mode');
-    searchInput.classList.toggle('dark-mode');
-    searchButton.classList.toggle('dark-mode');
-
-}
-
-toggleButton.addEventListener('click', switchTheme);
+    toggleButton.addEventListener('click', switchTheme);
+})();
--- js/search.js
+++ js/search.js
@@ -1,11 +1,13 @@
-const searchForm = document.getElementById('search-bar-container');
-const searchInput = document.getElementById("search-input");
+(() => {
+    const searchForm = document.getElementById('search-bar-container');
+    const searchInput = document.getElementById("search-input");
 
-searchForm.addEventListener("submit", function(event) {
-    event.preventDefault();
-    const searchTerm = searchInput.value.trim().toLowerCase();
+    searchForm.addEventListener("submit", function(event) {
+        event.preventDefault();
+        const searchTerm = searchInput.value.trim().toLowerCase();
 
-    if (searchTerm) {
-      alert(`คุณกำลังค้นหาคำว่า: ${searchTerm}`);
-    }
-});
+        if (searchTerm) {
+            alert(`คุณกำลังค้นหาคำว่า: ${searchTerm}`);
+        }
+    });
+})();
